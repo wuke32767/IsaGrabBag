ca46c20891c5fb37551c430cadd80edcc31bd859
bigkahuna443 <13278973+bigkahuna443@users.noreply.github.com>
2022-10-05 14:43:43 -0400

    Fix waterboost controller
    It had an invalid constructor, was originally manually loaded. Oops!

--- Code/WaterBoostMechanic.cs
+++ Code/WaterBoostMechanic.cs
@@ -1,10 +1,12 @@
 ﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
 using Monocle;
 
 namespace Celeste.Mod.IsaGrabBag {
     [CustomEntity("isaBag/waterBoost")]
     public class WaterBoostMechanic : Entity {
-        public WaterBoostMechanic(EntityData _data) {
+        public WaterBoostMechanic(EntityData _data, Vector2 offset)
+            : base(_data.Position + offset) {
             GrabBagModule.GrabBagMeta.WaterBoost = _data.Bool("boostEnabled");
         }
     }
