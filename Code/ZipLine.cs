415fd20e2e651fbf5c02bd9cb73dd79bf92ed889
catapillie <espritox@gmail.com>
2022-11-03 18:03:34 +0100

    Add flag option to ziplines

--- Code/ZipLine.cs
+++ Code/ZipLine.cs
@@ -19,6 +19,7 @@ namespace Celeste.Mod.IsaGrabBag {
         private readonly float height;
         private readonly Sprite sprite;
         private readonly bool usesStamina;
+        private readonly string flag;
         private float speed;
         private bool grabbed;
 
@@ -37,6 +38,8 @@ namespace Celeste.Mod.IsaGrabBag {
             currentGrabbed = null;
             Depth = -500;
 
+            flag = _data.Attr("flag", null)?.Trim();
+
             sprite = GrabBagModule.sprites.Create("zipline");
             sprite.Play("idle");
             sprite.JustifyOrigin(new Vector2(0.5f, 0.25f));
@@ -51,6 +54,11 @@ namespace Celeste.Mod.IsaGrabBag {
             Player self = GrabBagModule.playerInstance;
             self.Ducking = false;
             self.Speed.Y = 0;
+
+            // set flag
+            string flag = currentGrabbed.flag;
+            if (!string.IsNullOrWhiteSpace(flag))
+                self.SceneAs<Level>().Session.SetFlag(flag);
         }
 
         public static void ZipLineEnd() {
