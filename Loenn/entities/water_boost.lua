8d154b7bb7d73a87bfa1f64e02e445b6b6fe153d
bigkahuna443 <13278973+bigkahuna443@users.noreply.github.com>
2024-09-07 18:08:22 -0400

    Added water boost controller

---
+++ Loenn/entities/water_boost.lua
@@ -0,0 +1,14 @@
+local waterBoost = {}
+
+waterBoost.name = "isaBag/waterBoost"
+waterBoost.depth = 0
+waterBoost.texture = "isafriend/helperimage"
+
+waterBoost.placements = {
+    name = "default",
+    data = {
+        boostEnabled = true
+    }
+}
+
+return waterBoost
