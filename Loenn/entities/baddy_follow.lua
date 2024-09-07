9ad1426dc29e216723349b5e1478350ae1b82ac9
bigkahuna443 <13278973+bigkahuna443@users.noreply.github.com>
2024-09-07 16:33:13 -0400

    Add Friendly Badeline
    Selection rectangle is different from Ahorn, but placement position doesn't matter with this entity

---
+++ Loenn/entities/baddy_follow.lua
@@ -0,0 +1,11 @@
+local baddyFollow = {}
+
+baddyFollow.name = "isaBag/baddyFollow"
+baddyFollow.depth = 0
+baddyFollow.justification = {0.5, 1.0}
+baddyFollow.texture = "isafriend/baddyAhorn"
+baddyFollow.placements = {
+    name = "default"
+}
+
+return baddyFollow
